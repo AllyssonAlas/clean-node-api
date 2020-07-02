d051d3198a56ce03537a5558146a3441b0058e3b
Allysson Alas <allyssonmgs@gmail.com>
2020-07-02 15:10:48 -0300

    ensure requests return given content-type when selected

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -8,4 +8,14 @@ describe('Content-Type Middleware', () => {
     })
     await request(app).get('/test-content-type').expect('content-type', /json/)
   })
+
+  test('Should return xml content-type if selected', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/)
+  })
 })
